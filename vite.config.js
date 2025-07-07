6a74a38f9fda1dabee365f939db781bf7765f044
Guillermo Costilla <gcostilla96@gmail.com>
2025-07-06 21:06:44 -0300

    vercel

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,14 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   },
   server: {
     port: 3000,
